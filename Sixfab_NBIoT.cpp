eaa1b83c2edda2e3c1504f88242ddb6dcb0a8d27
Yasin Kaya <yasinkaya.121@gmail.com>
2018-09-17 15:57:16 +0300

    updated Sixfab_NBIoT library cpp and header files

--- Sixfab_NBIoT.cpp
+++ Sixfab_NBIoT.cpp
@@ -1,19 +1,19 @@
 /*
-  arduinoiot.cpp - Library for Sixfab Arduino NBIoT Shield.
-  Created by Yasin Kaya (selengalp), july 5, 2018.
+  Sixfab_NBIoT.cpp 
+  -
+  Library for Sixfab Arduino NBIoT Shield.
+  -
+  Created by Yasin Kaya (selengalp), September 17, 2018.
 */
 
 #include "Sixfab_NBIoT.h"
 
-// determine board type
-// Arduino Geniuno / Uno or Mega
-#if defined(__AVR_ATmega328P__) || defined(__AVR_ATmega168__) || defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
-  #define BC95_AT Serial
-  SoftwareSerial DEBUG(10,11); // RX, TX
-#endif
-
 Sixfab_HDC1080 hdc1080;
 MMA8452Q accel; // I2C Address : 0x1C
+
+#if defined(__AVR_ATmega328P__) || defined(__AVR_ATmega168__) || defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
+  SoftwareSerial DEBUG(10,11); // RX, TX - 9600 baud rate
+#endif 
 
 // default
 SixfabNBIoT::SixfabNBIoT()
@@ -21,65 +21,64 @@ SixfabNBIoT::SixfabNBIoT()
 
 }
 
-// function for initializing bc95 module.
+/******************************************************************************************
+ *** Base Functions : Set or Clear Hardwares - Status Controls - Helper AT Functions  *****
+ ******************************************************************************************/
+
+// function for initializing BC95 module.
 void SixfabNBIoT::init()
 {
-      // setting pin directions
-  pinMode(BC95_ENABLE, OUTPUT);
+  // setting pin directions
   pinMode(USER_LED, OUTPUT);
   pinMode(RELAY, OUTPUT);
   pinMode(USER_BUTTON, INPUT);
   pinMode(ALS_PT19_PIN, INPUT);
-
+  
   enable();
 
   // setting serials
   BC95_AT.begin(9600);
   DEBUG.begin(9600);
+
   DEBUG.println("Module initializing");
+  delay(500); // wait until module ready.
 
   // HDC1080 begin
   hdc1080.begin(0x40);
   // mma8452q init 
   accel.init();
 
+  setAutoConnectConf(AUTO_ON);
+  setScrambleConf(SCRAMBLE_ON);
+
   sendATComm("ATE1","OK\r\n"); 
-  sendATComm("ATE1","OK\r\n"); 
-  sendATComm("AT","AT\r\nOK\r\n");
+  sendATComm("ATE1","OK\r\n");
+  sendATComm("AT","OK\r\n");
 }
 
-// power up bc95 module and peripherals
+// power up BC95 module and all peripherals from voltage regulator 
 void SixfabNBIoT::enable()
 {
+  pinMode(BC95_ENABLE, OUTPUT);
   digitalWrite(BC95_ENABLE,HIGH);
 }
 
-// power down bc95 module
+// power down BC95 module and all peripherals from voltage regulator 
 void SixfabNBIoT::disable()
 {
   digitalWrite(BC95_ENABLE,LOW);
 }
 
-// connect to base station of operator
-void SixfabNBIoT::connectToOperator()
-{
-  DEBUG.println("Trying to connect base station of operator...");
-  sendATComm("AT+CGATT=1","OK\r\n");
-  sendATComm("AT+CGATT?","+CGATT:1\r\n");
-  sendATComm("AT+CSQ","OK\r\n"); 
-}
-
 // send at comamand to module
 void SixfabNBIoT::sendATCommOnce(const char *comm)
 {
-
   BC95_AT.print(comm);
   BC95_AT.print("\r");
   //DEBUG.println(comm);
 }
 
 // function for sending at command to BC95_AT.
-void SixfabNBIoT::sendATComm(const char *command, const char *desired_reponse)
+const char* SixfabNBIoT::sendATComm(const char *command, const char *desired_reponse)
 {
   uint32_t timer;
   char response[AT_RESPONSE_LEN]; // module response for AT commands. 
@@ -105,6 +104,41 @@ void SixfabNBIoT::sendATComm(const char *command, const char *desired_reponse)
       delay(2);
       }
       if(strstr(response, desired_reponse)){
+        return response;
+        memset(response, 0 , strlen(response));
+        break;
+      }    
+  }
+}
+
+// function for sending data to BC95_AT.
+const char* SixfabNBIoT::sendDataComm(const char *command, const char *desired_reponse)
+{
+  uint32_t timer;
+  char response[AT_RESPONSE_LEN]; // module response for AT commands. 
+
+  memset(response, 0 , AT_RESPONSE_LEN);
+  BC95_AT.flush();
+
+  BC95_AT.print(command);
+ 
+  timer = millis();
+   while(true){
+    if(millis()-timer > timeout){
+      BC95_AT.print(command);
+      timer = millis();
+    }
+    char c;
+    int i = 0; 
+
+    while(BC95_AT.available()){
+      c = BC95_AT.read();
+      DEBUG.write(c);
+      response[i++]=c;
+      delay(2);
+      }
+      if(strstr(response, desired_reponse)){
+        return response;
         memset(response, 0 , strlen(response));
         break;
       }    
@@ -114,9 +148,55 @@ void SixfabNBIoT::sendATComm(const char *command, const char *desired_reponse)
 // function for reset BC95_AT module
 void SixfabNBIoT::resetModule()
 {
-  sendATCommOnce("AT+NRB");
-  DEBUG.println("Rebooting Module...");
-  delay(4000);
+  saveConfigurations();
+  delay(200);
+
+  digitalWrite(BC95_ENABLE,LOW);
+  delay(200);
+  digitalWrite(BC95_ENABLE,HIGH);
+  delay(200);
+}
+
+// Function for save configurations that be done in current session. 
+void SixfabNBIoT::saveConfigurations()
+{
+  sendATComm("AT&W","OK\r\n");
+}
+
+// Function for getting IMEI number
+const char* SixfabNBIoT::getIMEI()
+{
+  return sendATComm("AT+CGSN","OK\r\n");
+}
+
+// Function for getting firmware info
+const char* SixfabNBIoT::getFirmwareInfo()
+{
+  return sendATComm("AT+CGMR","OK\r\n");
+}
+
+//Function for getting hardware info
+const char* SixfabNBIoT::getHardwareInfo()
+{
+  return sendATComm("AT+CGMM","OK\r\n");
+}
+
+// Function for setting autoconnect feature configuration 
+void SixfabNBIoT::setAutoConnectConf(const char *autoconnect)
+{
+  strcpy(compose, "AUTOCONNECT,");
+  strcat(compose, autoconnect);
+  sendATComm(compose,"OK\r\n");
+  clear_compose();
+}
+
+// Function for setting scramble feature configuration 
+void SixfabNBIoT::setScrambleConf(const char *scramble)
+{
+  strcpy(compose, "AT+NCONFIG=CR_0354_0338_SCRAMBLING,");
+  strcat(compose, scramble);
+  sendATComm(compose,"OK\r\n");
+  clear_compose();
 }
 
 // function for getting ip_address
@@ -140,7 +220,7 @@ const char* SixfabNBIoT::getDomainName()
 // function for setting domain name
 void SixfabNBIoT::setDomainName(char *domain)
 {
-	strcpy(domain_name, domain);
+  strcpy(domain_name, domain);
 }
 
 // function for getting domain_name
@@ -152,7 +232,7 @@ const char* SixfabNBIoT::getPort()
 // function for setting port
 void SixfabNBIoT::setPort(char *port)
 {
-	strcpy(port_number, port);
+  strcpy(port_number, port);
 }
 
 // get timout in ms
@@ -167,19 +247,54 @@ void SixfabNBIoT::setTimeout(uint16_t new_timeout)
   timeout = new_timeout; 
 }
 
+
+/******************************************************************************************
+ *** Network Service Functions ************************************************************
+ ******************************************************************************************/ 
+
+// function for getting signal quality
+const char* SixfabNBIoT::getSignalQuality()
+{
+  return sendATComm("AT+CSQ","OK\r\n");
+}
+
+// connect to base station of operator
+void SixfabNBIoT::connectToOperator()
+{
+  DEBUG.println("\nTrying to connect base station of operator...");
+  sendATComm("AT+CGATT=1","OK\r\n");
+  delay(500);
+  sendATComm("AT+CGATT?","+CGATT:1\r\n");
+  
+  getSignalQuality(); 
+}
+
+/******************************************************************************************
+ *** TCP & UDP Protocols Functions ********************************************************
+ ******************************************************************************************/
+
+// function for connecting to server via UDP
+void SixfabNBIoT::startUDPService()
+{
+  char *port = "3005";
+
+  strcpy(compose, "AT+NSOCR=DGRAM,17,");
+  strcat(compose, port);
+  strcat(compose, ",0");
+  
+  sendATComm(compose,"OK\r\n");
+  clear_compose();
+}
+
 // fuction for sending data via udp.
 void SixfabNBIoT::sendDataUDP(const char *data)
 {
-  char compose[DATA_COMPOSE_LEN]="";
-  char data_hex[DATA_COMPOSE_LEN];
-  char data_len[DATA_LEN_LEN];
-  
-  for(int i= 0; i<strlen(data); i++){
-    sprintf(data_hex+i*2, "%02X", data[i]);
-    }  
-
+  char data_len[5];
   sprintf(data_len, "%d", strlen(data));
-  strcat(compose, "AT+NSOST=0,");
+
+  convert_data_str_to_hex(data);
+
+  strcpy(compose, "AT+NSOST=0,");
   strcat(compose, ip_address);
   strcat(compose, ",");
   strcat(compose, port_number);
@@ -187,11 +302,21 @@ void SixfabNBIoT::sendDataUDP(const char *data)
   strcat(compose, data_len);
   strcat(compose, ",");
   strcat(compose, data_hex);
-  DEBUG.print("\n"); 
-  DEBUG.println(compose);
-  
-  sendATComm(compose,"OK\r\n");
+
+  sendATComm(compose,"\r\n");
+  clear_compose();
+  clear_data_hex();
 }
+
+// function for closing server connection
+void SixfabNBIoT::closeConnection()
+{
+  sendATComm("AT+NSOCL=0","\r\n");
+}
+
+/******************************************************************************************
+ *** Peripheral Devices' Functions : Read sensors - Set Relay and LEDs ********************
+ ******************************************************************************************/  
 
 // 
 void SixfabNBIoT::readAccel(double* ax, double* ay, double* az)
@@ -249,3 +374,4 @@ void SixfabNBIoT::turnOffUserLED()
 {
   digitalWrite(USER_LED, LOW);
 }
+
--- Sixfab_NBIoT.h
+++ Sixfab_NBIoT.h
@@ -1,6 +1,9 @@
 /*
-  arduinoiot.h - Library for Sixfab Arduino NBIoT Shield.
-  Created by Yasin Kaya (selengalp), july 5, 2018.
+  Sixfab_NBIoT.cpp 
+  -
+  Library for Sixfab Arduino NBIoT Shield.
+  -
+  Created by Yasin Kaya (selengalp), September 17, 2018.
 */
 
 #ifndef _SIXFAB_NBIOT_H
@@ -23,16 +26,15 @@
 // determine board type
 // Arduino Geniuno / Uno or Mega
 #if defined(__AVR_ATmega328P__) || defined(__AVR_ATmega168__) || defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
-  #define BC95_AT Serial
-  extern SoftwareSerial DEBUG; // RX, TX
-
+  #define BC95_AT Serial // 115200 baud rate
+  extern SoftwareSerial DEBUG;
 // Tinylab, Arduino Leonardo or Micro  
 #elif defined(__AVR_ATmega32U4__) || defined(__AVR_ATmega16U4__)
-  #define BC95_AT Serial1 
+  #define BC95_AT Serial1 // 115200 baud rate
   #define DEBUG Serial
-
 #endif
 
+// Peripheral Pin Definations
 // Peripheral Pin Definations
 #define USER_BUTTON 8
 #define USER_LED 6
@@ -45,12 +47,16 @@
 #define IP_ADDRESS_LEN 30
 #define DOMAIN_NAME_LEN 50
 #define PORT_NUMBER_LEN 8  
-#define AT_COMM_LEN 200
-#define AT_RESPONSE_LEN 400
-#define DATA_COMPOSE_LEN 200
-#define DATA_LEN_LEN 5  
+#define AT_COMM_LEN 100
+#define AT_RESPONSE_LEN 100
+#define DATA_COMPOSE_LEN 100
+#define DATA_LEN_LEN 3  
 
+#define SCRAMBLE_ON "TRUE"
+#define SCRAMBLE_OFF "FALSE"
 
+#define AUTO_ON "TRUE"
+#define AUTO_OFF "FALSE"
 
 class SixfabNBIoT
 {
@@ -64,13 +70,16 @@ class SixfabNBIoT
     [no-param]
     */
     SixfabNBIoT();
-
+    
+/******************************************************************************************
+ *** Base Functions : Set or Clear Hardwares - Status Controls - Helper AT Functions  *****
+ ******************************************************************************************/
+    
     /*
     Function for initializing module and peripherals
     This function do things below:
-    * enables bc95 module
+    * enables BC95 module
     * sets pin directions
-
 
     [no-return]
     ---
@@ -79,7 +88,7 @@ class SixfabNBIoT
     void init(); // initialize
 
     /*
-    Function for powering up bc95 module
+    Function for powering up BC95 module and all peripherals from voltage regulator 
 
     [no-return]
     ---
@@ -88,7 +97,7 @@ class SixfabNBIoT
     void enable();
     
     /*
-    Function for powering down bc95 module
+    Function for powering down BC95 module and all peripherals from voltage regulator 
 
     [no-return]
     ---
@@ -97,16 +106,7 @@ class SixfabNBIoT
     void disable();
 
     /*
-    Function for connecting to base station of operator
-
-    [no-return]
-    ---
-    [no-param]
-    */
-    void connectToOperator();
-
-    /*
-    Function for sending AT [param #1] command to bc95.
+    Function for sending AT [param #1] command to BC95.
     
     [no-return]
     ---
@@ -115,25 +115,90 @@ class SixfabNBIoT
     void sendATCommOnce(const char *);
 
     /*
-    Function for sending AT [param #1] command to bc95. If the desired [param #2] 
+    Function for sending AT [param #1] command to BC95. If the desired [param #2] 
     response isn't recevived, function resend the AT command wait a time as [timeout].
     
-    [no-return]
+    [return] : const char* response of AT command that received from BC95 modem
     ---
     [param #1] : const char* AT command word
     [param #2] : const char* AT desired_response word
     [param #3] : const char* AT response word
     */
-    void sendATComm(const char *, const char *); 
+    const char* sendATComm(const char *, const char *); 
+    
+    /*
+    Function for sending Data [param #1] to BC95. If the desired [param #2] 
+    response isn't recevived, function resend the Data wait a time as [timeout].
+    
+    [return] : const char* response of Data that received from BC95 modem
+    ---
+    [param #1] : const char* Data word
+    [param #2] : const char* Data desired_response word
+    [param #3] : const char* Data response word
+    */
+    const char* sendDataComm(const char *, const char *);
 
     /*
-    Function for resetting b595 module
+    Function for resetting BC95 module and all peripherals.
 
     [no-return]
     ---
     [no-param]
     */
     void resetModule();
+
+   /*
+    Function to save configurations that be done in current session.   
+    
+    [no-return]
+    --
+    [no-param]
+    */
+    void saveConfigurations();
+
+    /*
+    Function for getting IMEI number
+
+    [return] : const char* IMEI number
+    [no-param]
+    */
+    const char* getIMEI();
+
+    /*
+    Function for getting firmware info
+
+    [return] : const char* firmware info
+    ---
+    [no-param]
+    */
+    const char* getFirmwareInfo();
+
+    /*
+    Function for getting hardware info
+
+    [return] : const char* hardware info
+    ---
+    [no-param]
+    */
+    const char* getHardwareInfo();
+
+    /*
+    Function for setting autoconnect feature configuration 
+
+    [no-return]
+    ---
+    [param #1] : const char * Scramble on / off ( '0' is on, '1' is off)
+    */
+    void setAutoConnectConf(const char *);
+
+    /*
+    Function for setting scramble feature configuration 
+
+    [no-return]
+    ---
+    [param #1] : const char * Scramble on / off ( '0' is on, '1' is off)
+    */
+    void setScrambleConf(const char *);
 
     /*
     Function for getting described ip address
@@ -207,6 +272,41 @@ class SixfabNBIoT
     */
     void setTimeout(uint16_t);
 
+/******************************************************************************************
+ *** Network Service Functions ************************************************************
+ ******************************************************************************************/ 
+
+    /*
+    Function for getting signal quality
+
+    [return] : const char* signal quality
+    ---
+    [no-param]
+    */
+    const char* getSignalQuality();
+
+   /*
+    Function for connecting to base station of operator
+
+    [no-return]
+    ---
+    [no-param]
+    */
+    void connectToOperator();
+   
+/******************************************************************************************
+ *** TCP & UDP Protocols Functions ********************************************************
+ ******************************************************************************************/
+
+    /* 
+    // function for connecting to server via UDP
+
+    [no-return]
+    ---
+    [no-param]
+    */
+    void startUDPService();
+
     /*
     Function for sending data via UDP protocol. 
     First use setIPAddress and setPort functions before 
@@ -216,20 +316,152 @@ class SixfabNBIoT
     */
     void sendDataUDP(const char *);
 
+    /* 
+    Function for closing server connection
+    
+    [no-return]
+    ---
+    [no-param]
+    */
+    void closeConnection();
+
+
+/******************************************************************************************
+ *** Peripheral Devices' Functions : Read sensors - Set Relay and LEDs ********************
+ ******************************************************************************************/    
+    
+    /* 
+    Function for reading accelerometer values.
+    
+    [no-return]
+    ---
+    [param #1] : double* acceleration on x plane
+    [param #2] : double* acceleration on y plane
+    [param #y] : double* acceleration on z plane
+    */
     void readAccel(double*, double*, double*);
+
+    /* 
+    Function for reading temperature.
+    
+    [return] : double temperature  (celcius)
+    ---
+    [no-param]
+    */
     double readTemp();
+
+    /* 
+    Function for reading humidity.
+    
+    [return] : double humidity %x
+    ---
+    [no-param]
+    */
     double readHum();
+
+    /* 
+    Function for reading raw adc data from light sensor.
+    
+    [return] : double light adc 0-1023
+    ---
+    [no-param]
+    */
     double readLux();
+
+    /* 
+    Function for turning on relay.
+    
+    [no-return]
+    ---
+    [no-param]
+    */
     void turnOnRelay();
+
+    /* 
+    Function for turning off relay.
+    
+    [no-return]
+    ---
+    [no-param]
+    */
     void turnOffRelay();
+
+    /* 
+    Function for reading button position.
+    
+    [return] : uint8_t button position 0 or 1
+    ---
+    [no-param]
+    */
     uint8_t readUserButton();
+
+    /* 
+    Function for turning on LED.
+    
+    [no-return]
+    ---
+    [no-param]
+    */
     void turnOnUserLED();
+
+    /* 
+    Function for turning off LED.
+    
+    [no-return]
+    ---
+    [no-param]
+    */
     void turnOffUserLED();
 
   private:
+    char compose[300];
+    char data_hex[200];
+
     char ip_address[IP_ADDRESS_LEN]; //ip address       
     char domain_name[DOMAIN_NAME_LEN]; // domain name   
     char port_number[PORT_NUMBER_LEN]; // port number 
     uint16_t timeout = TIMEOUT; // default timeout for function and methods on this library.
+
+/******************************************************************************************
+ *** Private Functions that be used in public methods, in order to ease the operations ****
+ ******************************************************************************************/
+    /* 
+    Function for clear command buffer #private param : compose[300].
+    
+    [no-return]
+    ---
+    [no-param]
+    */
+    void clear_compose()
+    {
+        memset(compose,0,sizeof(compose));
+    }
+
+    /* 
+    Function for clear data_hex #private param : data_hex[200].
+    
+    [no-return]
+    ---
+    [no-param]
+    */
+    void clear_data_hex()
+    {
+        memset(data_hex,0,sizeof(data_hex));
+    }
+
+    /* 
+    Function for convert string data to hex data
+    
+    [return] : const char * data_hex
+    ---
+    [no-param]
+    */
+    void convert_data_str_to_hex( const char *data )
+    {
+        int i;
+        for (i = 0; i < strlen(data); i++){
+            sprintf(data_hex+i*2, "%02X", data[i] );
+        }
+    }
 };
 #endif
